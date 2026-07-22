09140bfb59dfa63cd323f5175374fd697b00ace4
flpzht <flpcsb@hotmail.com>
2026-07-22 18:50:43 -0300

    feat: configure application architecture

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My Portfolio</h1>
+      {/* Add your portfolio components and content here */}
+    </div>
+  );
+}
+
+export default App;
