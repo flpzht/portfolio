2eda6bb69fac870a2b6680d254f9b5e16932449e
flpzht <flpcsb@hotmail.com>
2026-08-04 14:07:49 -0300

    feat(router): configure home page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+
 import MainLayout from '@/layouts/MainLayout/MainLayout';
+import Home from '@/pages/Home/Home';
 
 export default function App() {
   return (
-    <MainLayout>
-      <h1>Portfolio</h1>
-    </MainLayout>
+    <Routes>
+      <Route path="/" element={<MainLayout><Home /></MainLayout>} />
+    </Routes>
   );
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import styles from "./Home.module.css";
+
+export default function Home() {
+  return (
+    <section className={styles.home}>
+      <h1>Portfolio</h1>
+    </section>
+  );
+}
