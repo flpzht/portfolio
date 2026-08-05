7f8fc141b83bc111cecbc43f7428f6b25c9c0eb9
flpzht <flpcsb@hotmail.com>
2026-08-05 17:45:14 -0300

    feat(hero): create hero section layout

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -5,16 +5,26 @@ export default function Hero() {
     <section id='hero' className={styles.hero}>
 
       <div className={styles.container}>
+        <div className={styles.content}>
 
-        <p className={styles.greeting}>Olá, eu sou</p>
+          <p className={styles.greeting}>Olá, sou</p>
 
-        <h1 className={styles.name}>Felipe</h1>
+          <h1 className={styles.name}>Felipe</h1>
 
-        <h2 className={styles.role}>Full Stack Developer</h2>
+          <h2 className={styles.role}>Full Stack Developer</h2>
 
-        <p className={styles.description}>Desenvolvedor apaixonado por criar interfaces modernas,
-          acessíveis e performáticas utilizando React, JavaScript e
-          tecnologias web.</p>
+          <p className={styles.description}>Desenvolvedor apaixonado por criar interfaces modernas,
+            acessíveis e performáticas utilizando React, JavaScript e
+            tecnologias web.
+          </p>
+
+          <div className={styles.buttons}>
+            {/*Em desenvolvimento*/}
+          </div>
+        </div>
+        <div className={styles.image}>
+          {/* Em desenvolvimento */}
+        </div>
 
       </div>
     </section>
