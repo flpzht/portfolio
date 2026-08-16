f33fe64c516649ee9a39346b35bfa01e72b33086
flpzht <flpcsb@hotmail.com>
2026-08-16 18:24:05 -0300

    refactor(button): refactor button js and style

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,17 +3,34 @@ import styles from "./Button.module.css";
 export default function Button({
   children,
   href,
-  target = "_blank",
-  rel = "noopener noreferrer",
+  type = "button",
+  onClick,
+  target,
+  rel,
+  variant = "primary",
 }) {
+  const className = `${styles.button} ${styles[variant]}`;
+
+  if (href) {
+    return (
+      <a
+        href={href}
+        className={className}
+        target={target}
+        rel={rel}
+      >
+        {children}
+      </a>
+    );
+  }
+
   return (
-    <a
-      className={styles.button}
-      href={href}
-      target={target}
-      rel={rel}
+    <button
+      type={type}
+      className={className}
+      onClick={onClick}
     >
       {children}
-    </a>
+    </button>
   );
 }
